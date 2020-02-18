bc3eed5c640ae2826c5ff5f0dd46b02e3dd6e99c
Andrew Kachnic <ajkachnic@protonmail.com>
2020-02-18 17:19:28 -0500

    Bug Fix: Array Issue
    I had forgotten to add the index to some of the array usage and fixed it

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-(function() {
+(function () {
 
     var tl = prompt("Top Right Square Key:").toLowerCase() || "w";
     var tr = prompt("Top Left Square Key:").toLowerCase() || "e";
     var bl = prompt("Bottom Right Square Key:").toLowerCase() || "s";
     var br = prompt("Bottom Left Square Key:").toLowerCase() || "d";
-    
+
     document.addEventListener('keypress', function (e) {
         const key = e.key;
         // Top Left
         if (key.toLowerCase() === tl) {
-            document.getElementsByClassName("card-button__CardButton-vbewcy-1").click();
+            document.getElementsByClassName("card-button__CardButton-vbewcy-1").click[0]();
         }
         // Top Right
         else if (key.toLowerCase() === tr) {
             document.getElementsByClassName("card-button__CardButton-vbewcy-1")[1].click();
         }
-    // Bottom Left
-    else if (key.toLowerCase() === bl) {
-        document.getElementsByClassName("card-button__CardButton-vbewcy-1")[2].click();
-    }
-    // Bottom Right
-    else if (key.toLowerCase() === br) {
-        document.getElementsByClassName("card-button__CardButton-vbewcy-1")[3].click();
-    }
-})
+        // Bottom Left
+        else if (key.toLowerCase() === bl) {
+            document.getElementsByClassName("card-button__CardButton-vbewcy-1")[2].click[2]();
+        }
+        // Bottom Right
+        else if (key.toLowerCase() === br) {
+            document.getElementsByClassName("card-button__CardButton-vbewcy-1")[3].click[3]();
+        }
+    })
 })()
